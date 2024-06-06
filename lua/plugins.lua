1613a898206a07a53dd8dd8449be3d6b66bf5def
Mo Hyun <mo.hyun@outlook.com>
2024-06-06 15:21:30 -0700

    lazy: Add text objects

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,4 +61,10 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+
+  -- Text objects
+  "wellle/targets.vim",
+  { "glts/vim-textobj-comment", dependencies = { "kana/vim-textobj-user" } },
+  { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
+  { "kana/vim-textobj-indent", dependencies = { "kana/vim-textobj-user" } },
 }
