c3da19d27ff0b62158cf5d5daf6765115cb2e136
Mo Hyun <mo.hyun@outlook.com>
2024-06-06 13:55:04 -0700

    lazy: Add minor plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,7 @@
 return {
-  -- the colorscheme should be available when starting Neovim
+  -- GUI
   {
+    -- the colorscheme should be available when starting Neovim
     "tomasr/molokai",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
@@ -9,13 +10,23 @@ return {
       vim.cmd([[colorscheme molokai]])
     end,
   },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function() require("colorizer").setup({ "*" }, { names = false }) end,
+  },
 
+  -- General
+  { "vim-scripts/DoxygenToolkit.vim", cmd = "Dox" },
   {
     "numToStr/Comment.nvim",
     opts = {},
     lazy = false,
   },
-
+  {
+    "sjl/gundo.vim",
+    init = function() vim.g.gundo_prefer_python3 = 1 end,
+    keys = { { "<F5>", "<Cmd>GundoToggle<CR>" } },
+  },
   {
     "scrooloose/nerdtree",
     init = function() vim.g.NERDTreeHijackNetrw = 0 end,
@@ -25,7 +36,26 @@ return {
       { "<Leader>n", "<Cmd>NERDTree<CR>" },
     },
   },
-
+  { "davidgranstrom/nvim-markdown-preview", cmd = "MarkdownPreview" },
+  {
+    "yssl/QFEnter",
+    init = function()
+      vim.g.qfenter_keymap = { vopen = { "<C-v>" }, hopen = { "<C-s>" } }
+    end,
+    ft = "qf",
+  },
+  {
+    "voldikss/vim-floaterm",
+    keys = {
+      {
+        "<Leader>lg",
+        "<Cmd>FloatermNew --height=0.9 --width=0.9 --title=lazygit lazygit<CR>",
+      },
+    },
+  },
+  "tpope/vim-surround",
+  "tpope/vim-unimpaired",
+  "tpope/vim-vinegar",
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
