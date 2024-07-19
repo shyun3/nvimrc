cba3d41c92adaec2ddb2d2e7431fbbdf9be67e1c
Mo Hyun <mo.hyun@outlook.com>
2024-07-19 15:53:48 -0700

    Disable LSP signature hint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,11 +108,7 @@ return {
     opts = {
       toggle_key = "<A-x>",
 
-      hint_prefix = {
-        above = "↙ ", -- when the hint is on the line above the current line
-        current = "← ", -- when the hint is on the same line
-        below = "↖ ", -- when the hint is on the line below the current line
-      },
+      hint_enable = false,
     },
 
     keys = {
