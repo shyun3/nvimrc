61ad510eb5547de5b6995362d847f857856c4ee2
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:34 -0700

    lazy: Add treesitter

--- init.lua
+++ init.lua
@@ -4,10 +4,6 @@
 vim.cmd.Plug(
   [['neoclide/coc.nvim', { 'branch': 'master', 'do': 'yarn install --frozen-lockfile' }]]
 )
-vim.cmd.Plug([['nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' }]])
-
--- Dependencies
-vim.cmd.Plug([['nvim-lua/plenary.nvim']])
 
 ]=]
 
@@ -292,38 +288,5 @@ vim.cmd.highlight("link CocSemTypeTypeParameter NONE")
 
 -- Taken from coc.vim
 vim.cmd.highlight("CocMenuSel ctermbg=237 guibg=#13354A")
-
--------------------------------------------------------------------------------
--- treesitter
-require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
-
-  highlight = {
-    enable = true,
-
-    disable = function(lang, buf)
-      if lang == "c" then return true end
-
-      -- disable slow treesitter highlight for large files
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then return true end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-})
 
 ]=]
