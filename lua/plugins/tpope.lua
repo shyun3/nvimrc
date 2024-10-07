e4b9f8b241e8342d67449cf574fbcdad3b781bec
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:50 -0700

    lazy: Add remaining tpope plugins

---
+++ lua/plugins/tpope.lua
@@ -0,0 +1,9 @@
+return {
+  { "tpope/vim-endwise", init = function() vim.g.endwise_no_mappings = 1 end },
+  "tpope/vim-fugitive",
+  "tpope/vim-projectionist",
+  "tpope/vim-repeat",
+  "tpope/vim-surround",
+  "tpope/vim-unimpaired",
+  "tpope/vim-vinegar",
+}
