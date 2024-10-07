5c53c50c9f6759f29bf3c512e2b7b550270d47dc
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:35 -0700

    conform: Don't notify on autoformat error

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -43,7 +43,7 @@ return {
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 500, lsp_fallback = true }
+      return { timeout_ms = 500, lsp_fallback = true, quiet = true }
     end,
   },
 
