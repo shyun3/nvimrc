0a36dcf6fc132a19183ff1c721fe26d5ee434f42
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:35 -0700

    lsp: Disable semantic highlighting for Lua
    Prefer treesitter highlighting.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,15 @@ return {
   config = function()
     require("mason-lspconfig").setup_handlers({
       function(server_name) require("lspconfig")[server_name].setup({}) end,
+
+      lua_ls = function()
+        require("lspconfig").lua_ls.setup({
+          -- Derived from `:h vim.lsp.semantic_tokens.start()`
+          on_attach = function(client)
+            client.server_capabilities.semanticTokensProvider = nil
+          end,
+        })
+      end,
     })
   end,
 }
