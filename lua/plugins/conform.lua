3721cb92abd5ffad2062145860233fafd5b7f319
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:35 -0700

    conform: Enable format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
         prepend_args = { "-style=file" },
       },
     },
+
     formatters_by_ft = {
       c = { "clang-format" },
       cpp = { "clang-format" },
@@ -35,6 +36,15 @@ return {
       lua = { "stylua" },
       python = { "isort", "black" },
     },
+
+    -- Derived from recipe
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
   },
 
   config = function(_, opts)
@@ -57,6 +67,20 @@ return {
         range = range,
       })
     end, { desc = "Format buffer", range = true })
+
+    -- Commands to enable/disable autoformatting, derived from recipe
+    vim.api.nvim_create_user_command("AutoFormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, { desc = "Disable autoformat-on-save", bang = true })
+    vim.api.nvim_create_user_command("AutoFormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { desc = "Re-enable autoformat-on-save" })
   end,
 
   -- Derived from lazy loading recipe
