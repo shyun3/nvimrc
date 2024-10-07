d7434f09a9e2a2ea10a6aa820fd6943d30101739
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:35 -0700

    Add function parameter text objects

--- lua/plugins/text-objects.lua
+++ lua/plugins/text-objects.lua
@@ -2,4 +2,5 @@ return {
   { "glts/vim-textobj-comment", dependencies = { "kana/vim-textobj-user" } },
   { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
   { "kana/vim-textobj-indent", dependencies = { "kana/vim-textobj-user" } },
+  { "sgur/vim-textobj-parameter", dependencies = { "kana/vim-textobj-user" } },
 }
