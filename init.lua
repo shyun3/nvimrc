6554ea3bdc8129d96841a0889ce52ef0a1b0deed
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:34 -0700

    lazy: Add conform

--- init.lua
+++ init.lua
@@ -18,7 +18,6 @@ vim.cmd.Plug(
   [['neoclide/coc.nvim', { 'branch': 'master', 'do': 'yarn install --frozen-lockfile' }]]
 )
 vim.cmd.Plug([['numToStr/Comment.nvim']])
-vim.cmd.Plug([['stevearc/conform.nvim']])
 vim.cmd.Plug([['vim-scripts/DoxygenToolkit.vim']])
 vim.cmd.Plug([['ibhagwan/fzf-lua']])
 vim.cmd.Plug([['sjl/gundo.vim']])
@@ -564,64 +563,6 @@ vim.cmd.highlight("CocMenuSel ctermbg=237 guibg=#13354A")
 -------------------------------------------------------------------------------
 -- Comment.nvim
 require("Comment").setup()
-
--------------------------------------------------------------------------------
--- conform.nvim
-require("conform").setup({
-  formatters = {
-    ["clang-format"] = {
-      prepend_args = { "-style=file" },
-    },
-  },
-  formatters_by_ft = {
-    c = { "clang-format" },
-    cpp = { "clang-format" },
-    json = { "prettier" },
-    lua = { "stylua" },
-    python = { "isort", "black" },
-  },
-})
-
--- Command to run async formatting, taken from recipes
-vim.api.nvim_create_user_command("Format", function(args)
-  local range = nil
-  if args.count ~= -1 then
-    local end_line =
-      vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-    range = {
-      start = { args.line1, 0 },
-      ["end"] = { args.line2, end_line:len() },
-    }
-  end
-  require("conform").format({ async = true, lsp_fallback = true, range = range })
-end, { range = true })
-
--- Derived from https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#range-formatting-with-a-motion
-function format_range_operator()
-  local old_func = vim.go.operatorfunc
-  _G.op_func_formatting = function()
-    require("conform").format({
-      async = true,
-      lsp_fallback = true,
-      range = {
-        start = vim.api.nvim_buf_get_mark(0, "["),
-        ["end"] = vim.api.nvim_buf_get_mark(0, "]"),
-      },
-    })
-
-    vim.go.operatorfunc = old_func
-    _G.op_func_formatting = nil
-  end
-  vim.go.operatorfunc = "v:lua.op_func_formatting"
-  vim.api.nvim_feedkeys("g@", "n", false)
-end
-
-vim.keymap.set(
-  { "n", "x" },
-  "<Leader>gf",
-  format_range_operator,
-  { desc = "Format selection", silent = true }
-)
 
 -------------------------------------------------------------------------------
 -- DoGe
