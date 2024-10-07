4cb4ce6c69b4231b653b9000375ca5cd9ee15bee
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:36 -0700

    Add lsp-overloads

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,34 @@
--- Derived from https://github.com/ray-x/lsp_signature.nvim/issues/228#issuecomment-1712704798
-local function scroll_sig(map)
-  local winnr = _G._LSP_SIG_CFG.winnr
-  local float_open = winnr and winnr ~= 0 and vim.api.nvim_win_is_valid(winnr)
-  if float_open then vim.fn.win_execute(winnr, ":normal! " .. map) end
+local function on_lsp_attach(client, bufnr)
+  -- Derived from `:h lsp-defaults-disable`
+  vim.bo.tagfunc = ""
+
+  if client.name == "lua_ls" then
+    -- Derived from `:h vim.lsp.semantic_tokens.start()`
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+
+  if client.server_capabilities.signatureHelpProvider then
+    ---@diagnostic disable-next-line: missing-fields
+    require("lsp-overloads").setup(client, {
+      ---@diagnostic disable-next-line: missing-fields
+      ui = { floating_window_above_cur_line = true },
+      keymaps = {
+        next_signature = "<C-n>",
+        previous_signature = "<C-p>",
+        next_parameter = "<C-l>",
+        previous_parameter = "<C-h>",
+        close_signature = "<C-s>",
+      },
+      display_automatically = false,
+    })
+
+    vim.keymap.set(
+      { "n", "i", "v" },
+      "<C-s>",
+      "<Cmd>LspOverloadsSignature<CR>",
+      { silent = true, buffer = bufnr }
+    )
+  end
 end
 
 return {
@@ -12,17 +38,10 @@ return {
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
+      "Issafalcon/lsp-overloads.nvim",
     },
 
     config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("my_lsp", {}),
-        callback = function()
-          -- Derived from `:h lsp-defaults-disable`
-          vim.bo.tagfunc = ""
-        end,
-      })
-
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend(
         "force",
@@ -34,17 +53,7 @@ return {
         function(server_name)
           require("lspconfig")[server_name].setup({
             capabilities = capabilities,
-          })
-        end,
-
-        lua_ls = function()
-          require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-
-            -- Derived from `:h vim.lsp.semantic_tokens.start()`
-            on_attach = function(client)
-              client.server_capabilities.semanticTokensProvider = nil
-            end,
+            on_attach = on_lsp_attach,
           })
         end,
       })
@@ -65,12 +74,14 @@ return {
 
       {
         "<Leader>gd",
+
         function()
           local diag = vim.diagnostic.get(0)
           local qf = vim.diagnostic.toqflist(diag)
           vim.fn.setqflist(qf)
           vim.cmd("botright copen")
         end,
+
         desc = "LSP: Buffer diagnostics",
       },
 
@@ -79,10 +90,12 @@ return {
 
       {
         "<Leader>ch",
+
         function()
           ---@diagnostic disable-next-line: missing-parameter
           vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
         end,
+
         desc = "LSP: Toggle inlay hints",
       },
     },
--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,13 +1,24 @@
 return {
   {
     "tpope/vim-endwise",
-    init = function() vim.g.endwise_no_mappings = 1 end,
+    init = function()
+      -- To disable <CR> from being mapped
+      vim.g.endwise_no_mappings = 1
+    end,
   },
 
   "tpope/vim-fugitive",
   "tpope/vim-projectionist",
   "tpope/vim-repeat",
-  "tpope/vim-surround",
+
+  {
+    "tpope/vim-surround",
+    init = function()
+      -- Mainly so that <C-s> can be used to show signature
+      vim.g.surround_no_insert_mappings = 1
+    end,
+  },
+
   "tpope/vim-unimpaired",
   "tpope/vim-vinegar",
 }
