fbb1f028dd19e68795c746bdd3dfbcc480f3e979
Mo Hyun <shyun3@yasld.com>
2024-10-07 09:29:50 -0700

    Fix project welcome screen conflicting with Lazy
    On startup, Lazy pops up a floating window if there are missing plugins
    to install. The welcome screen would erroneously display on this window.

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -5,9 +5,24 @@ return {
     vim.fn["project#rc"]()
     vim.cmd.source(vim.fn.stdpath("config") .. "/projects.vim")
 
-    -- Derived from project.vim
-    if vim.fn.argc(-1) == 0 and vim.fn.line2byte("$") == -1 then
-      vim.cmd.Welcome()
+    if vim.fn.argc(-1) == 0 then
+      local group = vim.api.nvim_create_augroup("project", {})
+
+      -- Show welcome screen on startup, making sure to display it on the main
+      -- window and not in the Lazy floating window which pops up when
+      -- installing missing plugins
+      vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter" }, {
+        group = group,
+        pattern = "*",
+        callback = function()
+          if vim.fn.winnr() ~= 1 then return end
+
+          -- Derived from project.vim
+          if vim.fn.line2byte("$") == -1 then vim.cmd.Welcome() end
+
+          vim.api.nvim_clear_autocmds({ group = group })
+        end,
+      })
     end
   end,
 }
