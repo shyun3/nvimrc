d8958faba736bec92692b058e160555be1b4ab81
Mo Hyun <shyun3@yasld.com>
2024-10-10 15:57:17 -0700

    polyglot: Disable markdown gx mapping

--- lua/plugins/polyglot.lua
+++ lua/plugins/polyglot.lua
@@ -13,14 +13,19 @@ return {
     -- polyglot sets this variable, which prevents loading of `filetype.lua`
     vim.g.did_load_filetypes = nil
 
+    ------------------------------------------------------------------------
     -- vim-cpp-modern
     vim.cmd.highlight("link cppSTLios NONE") -- STL I/O manipulators
     vim.cmd.highlight("link cppSTLconstant NONE") -- C++17 constants
     vim.cmd.highlight("link cppSTLtype NONE") -- C++17 STL types
     vim.cmd.highlight("link cppSTLconcept NONE") -- C++20 concepts
 
+    ------------------------------------------------------------------------
     -- vim-markdown
     vim.g.vim_markdown_auto_insert_bullets = 0
     vim.g.vim_markdown_new_list_item_indent = 0
+
+    -- Disable `gx` mapping
+    vim.keymap.set("", "<Plug>", "<Plug>Markdown_OpenUrlUnderCursor")
   end,
 }
